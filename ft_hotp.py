8e100407cd686fd07502b2cfa47fa82ae3a08c65
Yeray Gonzalez bartolome <ygonzale@c3r6s4.42urduliz.com>
2022-08-24 17:58:55 +0200

    mod

--- ft_hotp.py
+++ ft_hotp.py
@@ -4,19 +4,14 @@ from cryptography.fernet import Fernet
 
 
 def get_hotp_token(secret, intervals_no):
-
 	key = base64.b16decode(secret, True)
-
 	#decodificar la key
 	msg = struct.pack(">Q", intervals_no)
-
 	#conversiones entre valores de Python y estructuras C representadas
 	h = hmac.new(key, msg, hashlib.sha1).digest()
 	o = o = h[19] & 15
-
 	#genera un hash usando ambos. El algoritmo hash es HMAC
 	h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
-
 	#unpacking
 	return h
 
